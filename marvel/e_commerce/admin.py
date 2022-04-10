ddbd208651f3b03aff3393865f04cd1ec75d4b5e
franciscososa <franciscososa91@gmail.com>
2022-04-10 16:29:34 -0300

    modificaciones para levantar postgresql y comment admin fieldset

--- marvel/e_commerce/admin.py
+++ marvel/e_commerce/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
 
 # NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
-# from e_commerce.models import *
+from e_commerce.models import *
 
 # Register your models here.
 
 # NOTE: Aquí personalizamos los campos en el Django Admin.
 
-# @admin.register(Comic)
-# class ComicsAdmin(admin.ModelAdmin):
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
     # NOTE: Para seleccionar los campos en la tabla de registros
-    # list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
 
     # NOTE: Filtro lateral de elementos:
-    # list_filter= ('marvel_id','title')
+    list_filter= ('marvel_id','title')
     
     # NOTE: Buscador de elementos en la columna:
-    # search_fields = ['title']
+    search_fields = ['title']
 
     # NOTE: Para seleccionar los campos en el registro. 
-    # fields = ('marvel_id', 'title', 'stock_qty')
+    fields = ('marvel_id', 'title', 'stock_qty')
 
     # NOTE: Genera un campo desplegable con los registros seleccionados.
     # fieldsets = (
@@ -31,4 +31,9 @@ from django.contrib import admin
     #         'fields': ('description','price', 'picture'),
     #     }),
     # )
-    # pass
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    # NOTE: Para seleccionar los campos en la tabla de registros
+    list_display = ('user_id', 'comic_id', 'favorite', 'cart','wished_qty','buied_qty')
+    
